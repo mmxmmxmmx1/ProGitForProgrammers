fb61deea38a9d2b0ffad1a058664d376e0b1b6d7
mmxmmxmmx1 <mmxmmxmmx1@gmail.com>
2025-05-07 22:24:24 +0800

    add a java code in helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -3,5 +3,6 @@ public class helloworld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         System.out.println("I just added this in VS Code");
+        System.out.println("I just added this in VS Code");
     }
 }
